2b2683a2251d442d058a2860964294bf4363399f
al18nna <melnik.alena2006@gmail.com>
2024-10-05 22:52:38 +0300

    Объединение двух репозиториев

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace module_3
     {
         static void Main(string[] args)
         {
+            string name = "James";
+            Console.WriteLine(name);
+            name = "Alena";
+            Console.WriteLine(name);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+            Console.Clear();
+
+            Console.WriteLine($"My name is {name}");
+
+            int age = 18;
+            Console.WriteLine($"I am {age} years old");
+
+            bool HavePet = false;
+            Console.WriteLine($"Do I have a pet? {HavePet}");
+
+            double size = 36.5;
+            Console.WriteLine($"My shoes size is {size}");
+            Console.Clear();
+
             DaysOfWeek MyFavoritDay = DaysOfWeek.Friday;
 
             Console.WriteLine(MyFavoritDay);
@@ -18,11 +38,12 @@ namespace module_3
 
             double result = 10 % 3;
             Console.WriteLine(result);
+            Console.Clear();
 
             Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
             Console.Write("Enter your age: ");
-            byte age = checked ((byte)int.Parse(Console.ReadLine()));
+            age = checked ((byte)int.Parse(Console.ReadLine()));
             Console.WriteLine("Your name is {0} and age is {1}. ", name, age);
             Console.Write("What's your favorite day of week? ");
             DaysOfWeek FavoritDay = (DaysOfWeek) byte.Parse(Console.ReadLine());
